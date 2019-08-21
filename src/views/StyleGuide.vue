8dfe2b895403834fd42a119765b4160b7e1adccf
Kim LaRocca <kimlarocca@me.com>
2019-08-21 16:48:20 -0400

    ugh

--- src/views/StyleGuide.vue
+++ src/views/StyleGuide.vue
@@ -114,7 +114,7 @@
     <hero
       class="full-screen smallHeader"
       image="assets/images/hero1.jpg"
-      mobileImage="../src/assets/images/hero1mobile.jpg"
+      mobileImage="assets/images/hero1mobile.jpg"
       accentColor="#ff8f2a"
       hasH1="true"
     >
@@ -425,43 +425,51 @@
       hasText - if this is set, plain text will display
       -->
 
-      <div class="grid-x grid-margin-x grid-margin-y sponsorship">
-        <div class="cell large-3 small-12 sponsorship-text">
-          <p class="text-brown">BROUGHT TO YOU BY</p>
-        </div>
-        <div class="cell large-9 sponsorship-container">
-          <sponsorship-callout
-            image="https://i.ibb.co/rkqb4SV/neiman-marcus.png"
-            altText="Neiman Marcus Logo"
-          >
-          </sponsorship-callout>
-          <sponsorship-callout
-            image="https://i.ibb.co/GFG4Wwy/chase.png"
-            altText="Chase Logo"
-          >
-          </sponsorship-callout>
-          <sponsorship-callout
-            image="https://i.ibb.co/XVB8KRW/chevron.png"
-            altText="Chevron Logo"
-          >
-          </sponsorship-callout>
-          <sponsorship-callout
-            image="https://i.ibb.co/4msnjPx/first-republic-bank.png"
-            altText="First Republic Bank"
-          >
-          </sponsorship-callout>
-          <sponsorship-callout
-            image="https://i.ibb.co/CsfRDXx/air-bnb.png"
-            altText="Airbnb Logo"
-          >
-          </sponsorship-callout>
-          <sponsorship-callout
-            hasText="LOREM IPSUM DOLOR SIT AT MET"
-          >
-          </sponsorship-callout>
+    </div>
 
+    <section class="bg-medium-gray">
+      <div class="container smaller">
+        <div class="grid-x grid-margin-x grid-margin-y sponsorship">
+          <div class="cell large-3 small-12 sponsorship-text">
+            <p class="text-brown">BROUGHT TO YOU BY</p>
+          </div>
+          <div class="cell large-9 sponsorship-container">
+            <sponsorship-callout
+              image="https://i.ibb.co/rkqb4SV/neiman-marcus.png"
+              altText="Neiman Marcus Logo"
+            >
+            </sponsorship-callout>
+            <sponsorship-callout
+              image="https://i.ibb.co/GFG4Wwy/chase.png"
+              altText="Chase Logo"
+            >
+            </sponsorship-callout>
+            <sponsorship-callout
+              image="https://i.ibb.co/XVB8KRW/chevron.png"
+              altText="Chevron Logo"
+            >
+            </sponsorship-callout>
+            <sponsorship-callout
+              image="https://i.ibb.co/4msnjPx/first-republic-bank.png"
+              altText="First Republic Bank"
+            >
+            </sponsorship-callout>
+            <sponsorship-callout
+              image="https://i.ibb.co/CsfRDXx/air-bnb.png"
+              altText="Airbnb Logo"
+            >
+            </sponsorship-callout>
+            <sponsorship-callout
+              hasText="LOREM IPSUM"
+            >
+            </sponsorship-callout>
+
+          </div>
         </div>
       </div>
+    </section>
+
+    <div class="container">
 
       <p class="subhead5 padding">Footer</p>
       <div class="padding-smaller"></div>
