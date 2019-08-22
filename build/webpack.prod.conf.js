3639532c9f60f2a87380927a027c265ab569aadf
Kim LaRocca <kimlarocca@me.com>
2019-08-22 14:35:43 -0400

    prerendering test 1

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -10,6 +10,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin')
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const PrerenderSpaPlugin = require('prerender-spa-plugin')
 
 const env = process.env.NODE_ENV === 'testing'
   ? require('../config/test.env')
@@ -36,6 +37,12 @@ const webpackConfig = merge(baseWebpackConfig, {
     new webpack.DefinePlugin({
       'process.env': env
     }),
+    new PrerenderSpaPlugin(
+      // Path to compiled app
+      path.join(__dirname, '../dist'),
+      // List of endpoints you wish to prerender
+      [ '/','/HomePage','/Hero','/MiniHero','/EventCard','/CategoryCard','/PersonProfile','/ContentBlade','/ConcertDetail','/MediaBlocks','/Accordions','/AboutPage','/Carousel' ]
+    ),
     new UglifyJsPlugin({
       uglifyOptions: {
         compress: {
--- src/router/index.js
+++ src/router/index.js
@@ -17,6 +17,7 @@ import Carousel from '../views/Carousel'
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
